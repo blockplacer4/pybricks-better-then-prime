1fa09058b5ad0268554495c755bd59748fa1c831
blockplacer4 <99276350+blockplacer4@users.noreply.github.com>
2024-09-20 15:51:40 +0200

    Update debug.py

--- debug.py
+++ debug.py
@@ -1,13 +1,10 @@
-from pybricks.hubs import PrimeHub
 from pybricks.pupdevices import Motor, ColorSensor, UltrasonicSensor, ForceSensor
 from pybricks.parameters import Button, Color, Direction, Port, Side, Stop
 from pybricks.robotics import DriveBase
 from pybricks.tools import wait, StopWatch
  
-hub = PrimeHub()
- 
 class debug:
-    def __init__(motorA, motorB, motorC, motorD):
+    def __init__(self, motorA, motorB, motorC, motorD, speed, angle):
         '''
         motorA: port for the first Motor
         motorB: port for the second Motor
@@ -15,47 +12,56 @@ class debug:
         motorD: port for the fourth Motor
         forceSensor: port for the force sensor
         '''
+        self.speed = speed
+        self.angle = angle
         self.motorA = Motor(port=motorA)
         self.motorB = Motor(port=motorB)
         self.motorC = Motor(port=motorC)
         self.motorD = Motor(port=motorD)
    
-    def turnA(direction):
+    def turnA(self, direction):
         '''
         direction: turn direction. 1 is clockwise, 2 is counterclockwise
         '''
+        print("Motor A")
         if direction == 1:
-            angle = 100
+            angle = self.angle
         else:
-            angle = -100
-        await self.motorA.run_angle(angle)
+            angle = self.angle - 2*self.angle
+        self.motorA.run_angle(self.speed, angle)
    
-    def turnB(direction):
+    def turnB(self, direction):
         '''
         direction: turn direction. 1 is clockwise, 2 is counterclockwise
         '''
+        print("Motor B")
         if direction == 1:
-            angle = 100
+            angle = self.angle
         else:
-            angle = -100
-        await self.motorB.run_angle(angle)
+            angle = self.angle - 2*self.angle
+        self.motorB.run_angle(self.speed, angle)
    
-    def turnC(direction):
+    def turnC(self, direction):
         '''
         direction: turn direction. 1 is clockwise, 2 is counterclockwise
         '''
+        print("Motor C")
         if direction == 1:
-            angle = 100
+            angle = self.angle
         else:
-            angle = -100
-        await self.motorC.run_angle(angle)
+            angle = self.angle - 2*self.angle
+        self.motorC.run_angle(self.speed, angle)
    
-    def turnD(direction):
+    def turnD(self, direction):
         '''
         direction: turn direction. 1 is clockwise, 2 is counterclockwise
         '''
+        print("Motor D")
         if direction == 1:
-            angle = 100
+            angle = self.angle
         else:
-            angle = -100
-        await self.motorD.run_angle(angle)
+            angle = self.angle - 2*self.angle
+        self.motorD.run_angle(self.speed, angle)
+
+    def temp(self):
+        pass
